810859713a56a157430b1c0ecd66134429580fd0
Pranil Zaman <pranilzaman@utexas.edu>
2023-06-28 09:37:53 -0500

    second commit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else: 
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "This entry does not exist."
+        })
+    else: 
+        return render(request, "encyclopedia/entry.html", {
+            "title": title, 
+            "content": html_content
+        })
+    
+
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry_search, 
+                "content": html_content
+            })
 
